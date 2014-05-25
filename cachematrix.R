80e96d3d1d036c8914b8bf7b5a363888eed5e8f9
Carlos P Montoya <cpmontoya@verizon.net>
2014-05-25 01:16:19 -0400

    wrote makeCacheMatix with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below can be used to create a matrix and solve for its inverse
+## the inverse matrix is cached for later retrieval. Caching the matrix makes
+## use of R's Lexical Scoping
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a list of functions for getting and
+## and setting a matrix as well as getting and setting its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     inv <- NULL
+     set <- function(temp){
+          x <<- temp
+          inv <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(solve) inv <<- solve
+     getinverse <- function() inv
+     list(set = set, get = get, setinverse = setinverse, 
+          getinverse = getinverse)
 }
 
 
